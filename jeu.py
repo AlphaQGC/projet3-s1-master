f84c36dd86d0c253c9ad9428f36b3479dddd36d8
Legrix <alegrix@gaming.tech>
2022-11-08 19:58:32 +0100

    jeu finis

--- jeu.py
+++ jeu.py
@@ -10,7 +10,7 @@
 def jeu(x):
     #appeler input et l'assigné dans une variable
     lettre = input("devine la lettre :")
-    #variable compteur 
+    #variable compteur = 0
     compteur = 0
     #Tant que :  x n'est pas egale au string que a renvoyé input
     while x != lettre :
@@ -18,12 +18,31 @@ def jeu(x):
         lettre = input()
         # on ajoute 1 à compteur a chaque fois que input est rappeler 
         compteur = compteur + 1
-    #input est egale à x
-    if lettre == x :
-        #alors renvoyer un message victoire
-        print("victoire")
-        #retourner vide
-        return compteur 
+        #retourner compteur
+        print(compteur)
+    
+    #alors renvoyer un message victoire
+    print("victoire")
+    return 
 
-jeu(a)
+#CORRECTION
+
+#je definis un mini jeu
+def miniJeux(x):
+    compteur = 0
+    #je definis une variable y qui sera le input
+    lettre = input ("Devine la lettre")
+    #tant que y est different de x
+    while y!= x :
+        #ajouter 1 au compteur
+        compteur = compteur + 1
+        #afficher le compteur
+        print(compteur)
+        #Alors j'appelle un nouveau y 
+        lettre = input("Devine la lettre")
+    #lorsque la variable y est egale a la variable x alors
+    print("reussi")
+
+
 #FIN
+
