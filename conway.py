0ef46dba9d6d54a670b5b7333d4b3bdccbded47e
Legrix <alegrix@gaming.tech>
2022-11-10 12:50:42 +0100

    update

--- conway.py
+++ conway.py
@@ -1,5 +1,14 @@
 #DEBUT
 
+def generateTable(L):
+    return [[(str(x) + '_' + str(y)) for x in range(L)] for y in range(L)]
 
+def displayTable(table):
+    for i in table:
+        print(i)
+
+connwayBoard = generateTable(5)
+
+displayTable(connwayBoard)
 
 #FIN
