326af2e3fba3b905013eacedec80dbf6e2411c91
Legrix <alegrix@gaming.tech>
2022-11-14 10:13:04 +0100

    update code

--- pierreFeuillePapierCiseau_FINAL.py
+++ pierreFeuillePapierCiseau_FINAL.py
@@ -1,30 +1,58 @@
 #DEBUT
 
 #On admet que la fonction random renvoie une donnée au hasard
+from random import *
 #On admet que la fonction input qui nous permet de demander à l'utilisateur une réponse
-#Definir game une fonction qui est un pierre papier ciseau 
+input('test de fonction')
+#Definir game une fonction qui est un pierre papier ciseau
+def game (): 
     #on creer un tableau choixJeu qui contient les 3 choix du jeu donc : "pierre", "papier", "ciseau"
-    #on creer une variable pointJoueur qui est egale a 0 
+    choixJeu = ["pierre","papier","ciseau : " ]
+    #on creer une variable pointJoueur qui est egale a 0
+    pointJoueur = 0 
     #on creer une variable pointOrdinateur qui est egale a 0
+    pointOrdinateur = 0
     #Tant que pointJoueur ou pointOrdianteur est different de 3 
-        #alors on creer une variable iaChoix qui est egale a un random de 1 à 3 de l'index du tableau choixJeu 
+    while pointJoueur or pointOrdinateur != 3 :
+        #alors on creer une variable iaChoix qui est egale a un random de 1 à 3 de l'index du tableau choixJeu
+        iaChoix = choice(choixJeu) 
         #on creer une variable monChoix en utilisant la fonction input avec en description les differentes réponse possible pour le joueur qui sont : "pierre", "papier", "ciseau" et assigne a notre variable monChoix le retour de l'execution de la fonction input
+        monChoix = input("Choisir une des réponses suivantes :pierre, papier, ciseau : " )
         #on affiche le contenu de la variable iaChoix pour connaitre le choix de l'ordinateur
+        print(iaChoix)
         #Si mon choix est different de iaChoix
+        if monChoix != iaChoix :
             #alors
             #Si monChoix est identique à "papier" et que iaChoix est different de "ciseau"
+            if monChoix == "papier" and iaChoix != "ciseau" :
                 #alors on ajoute un point au joueur
+                pointJoueur = pointJoueur + 1
                 #Sinon on ajoute un point à l'ordinateur
+            
             #Sinon si monChoix est identique à "pierre" et iaChoix est different de "papier"
+            elif monChoix == "pierre" and iaChoix != "papier" :
                 #alors on ajoute un point au joueur
+                pointJoueur = pointJoueur + 1
                 #Sinon on ajoute un point à l'ordinateur
+                
             #Sinon si monChoix est identique à "ciseau" et iaChoix different de "pierre"
+            elif monChoix == "ciseau" and iaChoix != "papier" :
                 #alors on ajoute un point au joueur
+                pointJoueur = pointJoueur + 1
                 #Sinon on ajoute un point à l'ordinateur
+            else :
+                pointOrdinateur = pointOrdinateur + 1
         #Sinon renvoyer "égalité pour ce tour, recommencez !" à l'utilisateur 
+        else :
+            print("égalité pour ce tour, recommencez !")
     #Si pointJoueur egale à 3
+    if pointJoueur == 3 :
         #alors on renvoie "Vous avez gagné ! :)"
+        print("Vous avez gagné ! :)")
     #Sinon on renvoie "Vous avez perdu ! :("
+    elif pointOrdinateur == 3 :
+        print("Vous avez perdu ! :(")
 
 #Executer la fonction game pour lancer le pierre, papier, ciseau
+game()
 #FIN
